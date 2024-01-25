dbae43b5362552e7ac58fcdf93b50e0070b9c51b
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-01-25 23:48:07 +0500

    homework

---
+++ js/script.js
@@ -0,0 +1,102 @@
+// 1-masala
+
+// function name(arg) {
+//    let n;
+//    n=arg**2
+//    return n;
+// }
+// console.log(name(7));
+
+
+// 2-masala
+
+
+// function max(arg1, arg2) {
+//     let n;
+//     if (arg1>arg2) {
+//         n=arg1**2;
+//     } else {
+//         n=arg2**2;
+//     }
+
+//     return n;
+// }
+// console.log(max(2, 3));
+
+
+
+// 3-masal
+
+// function yigindi(number) {
+//     let birlar = number % 10;
+//     let onlar = Math.floor((number % 100) / 10);
+//     let yuzlar = Math.floor(number / 100);
+//     let natija = birlar + onlar + yuzlar;
+//     return natija;
+
+// }
+// console.log(yigindi(256));
+
+
+
+//  4 - masala
+// function boluvchilari(arg) {
+//     let result = 0;
+//     for (let i = 1; i <= arg; i++) {
+//         if (arg % i === 0) {
+//             result++;
+//         }
+//     }
+//     return result;
+// }
+// console.log(boluvchilari(120));
+
+
+
+
+// 5-masala
+
+
+// function nam(n) {
+//     let sum =0;
+//     for (let i = 1; i <=n; i+=2) {
+//        sum+=i
+//     }
+//     return sum;
+// }
+// console.log(nam(5));
+
+// 6-masala
+
+// function nam(a, b) {
+//     let n=1;
+//     for (let i = a; i <=b; i++) {
+//        if (i%15==0) {
+//         n=n*i;
+//        }
+//     }
+//     return n;
+// }
+// console.log(nam(14,24));
+
+// 7-masala
+
+// function name(n) {
+//     let sum=0;
+//     let result;
+//     for (let i = 0; i <=n; i++) {
+//        if (n%i==0) {
+//         sum++;
+//        }
+
+//     }
+//     if (sum==2) {
+//         console.log("tub son");
+//     } else {
+//         console.log("emas");
+//     }
+//     return sum;
+// }
+
+// console.log(name(17));
+
